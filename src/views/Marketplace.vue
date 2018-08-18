e8b834addc44ab514b8c3240d5d1cb4916d81d7a
Adrian Mejia <admejiar@cisco.com>
2018-08-17 21:19:31 -0400

    render marketplace

---
+++ src/views/Marketplace.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="marketplace">
+    <div class="card" v-for="product in products" :key="product.id">
+      {{ product.name }} - ${{ product.price }}
+    </div>
+  </section>
+</template>
