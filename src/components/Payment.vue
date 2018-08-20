0184bbf5a9d8fc021902643195eaf1b4ce05bd3b
Adrian Mejia <admejiar@cisco.com>
2018-08-20 15:40:27 -0400

    placeholder for braintree payment component

---
+++ src/components/Payment.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div id="dropin-container"></div>
+    <button id="submit-button">checkout</button>
+  </section>
+</template>
+
+<script>
+// https://developers.braintreepayments.com/guides/drop-in/setup-and-integration/javascript/v3
+import dropin from 'braintree-web-drop-in';
+
+export default {
+  name: 'payment',
+
+  data() {
+    return {
+      instance: dropin.create({ /* options */ }),
+    };
+  },
+};
+</script>
+
