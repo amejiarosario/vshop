b72244df5fb00a9dcfededd733bce8f0bf543828
Adrian Mejia <admejiar@cisco.com>
2018-08-26 11:28:54 -0400

    fixing toolbar WIP

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,6 +10,7 @@ import {
   VGrid,
   VToolbar,
   transitions,
+  VTextField,
 } from 'vuetify';
 import 'vuetify/src/stylus/app.styl';
 
@@ -24,14 +25,15 @@ Vue.use(Vuetify, {
     VGrid,
     VToolbar,
     transitions,
+    VTextField,
   },
   theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
+    primary: '#344955', // Bue 700 | #37474F blue-grey darken-3
+    secondary: '#F9AA32', // Orange 500 | amber #FFC107
+    accent: '#82B1FF', // amber accent-1 #FFE57F
     error: '#FF5252',
     info: '#2196F3',
     success: '#4CAF50',
-    warning: '#FFC107',
+    warning: '#FF5722', // FFC107 |  red lighten-4 #FFCDD2 | deep-orange  #FF5722
   },
 });
